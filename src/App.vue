8e789a1aa133d0544ca3cd21717c1e5b6b3ab3de
clark-cui <cuirongchuan@163.com>
2021-03-09 20:13:18 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,57 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    </div> -->
+    <div id="svgContainer"></div>
+    <!-- <router-view /> -->
   </div>
 </template>
 
+<script lang="ts">
+import lottie from "lottie-web";
+export default {
+  mounted() {
+    const svgContainer = document.getElementById("svgContainer");
+    const animItem = lottie.loadAnimation({
+      wrapper: svgContainer,
+      animType: "svg",
+      loop: true,
+      path:
+        "https://labs.nearpod.com/bodymovin/demo/markus/isometric/markus2.json",
+    });
+  },
+};
+</script>
+
 <style lang="scss">
+html {
+  margin: 0;
+  padding: 0;
+  background-color: #1e1c26;
+}
+
+body,
+html {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  background-color: #000;
+  margin: 0;
+}
+
+#svgContainer {
+  max-width: 100%;
+  max-height: 100%;
+  background-color: #1e1c26;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
